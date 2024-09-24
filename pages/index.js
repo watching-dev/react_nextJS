88ff2c5bea0a16ebf4b650f096054402f37d2d13
watching-dev <coderefactoring90@gmail.com>
2024-09-24 23:18:01 +0900

    [feat] hero 컴포넌트 디자인

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
+import Hero from "../components/home-page/hero";
 import { Fragment } from "react";
 
 function HomePage() {
   return (
     <Fragment>
       <Hero />
-      <FeaturePosts />
+      {/* <FeaturePosts /> */}
     </Fragment>
   );
 }
