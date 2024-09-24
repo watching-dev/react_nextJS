414e234a2dba2dcc803d9d31114eb4cd4a475f38
watching-dev <coderefactoring90@gmail.com>
2024-09-24 22:23:39 +0900

    [fix] 앱 실행 문제 해결(ERR_PACKAGE_PATH_NOT_EXPORTED)_npm install -> npm audit fix --force -> package.json -> react: ^18.3.1, -> react-dom: ^18.3.1 -> npm install -> <a> 문구 삭제

--- components/posts/post-item.js
+++ components/posts/post-item.js
@@ -1,15 +1,15 @@
-import Link from 'next/link';
-import Image from 'next/image';
+import Link from "next/link";
+import Image from "next/image";
 
-import classes from './post-item.module.css';
+import classes from "./post-item.module.css";
 
 function PostItem(props) {
   const { title, image, excerpt, date, slug } = props.post;
 
-  const formattedDate = new Date(date).toLocaleDateString('en-US', {
-    day: 'numeric',
-    month: 'long',
-    year: 'numeric',
+  const formattedDate = new Date(date).toLocaleDateString("en-US", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
   });
 
   const imagePath = `/images/posts/${slug}/${image}`;
@@ -18,22 +18,22 @@ function PostItem(props) {
   return (
     <li className={classes.post}>
       <Link href={linkPath}>
-        <a>
-          <div className={classes.image}>
-            <Image
-              src={imagePath}
-              alt={title}
-              width={300}
-              height={200}
-              layout='responsive'
-            />
-          </div>
-          <div className={classes.content}>
-            <h3>{title}</h3>
-            <time>{formattedDate}</time>
-            <p>{excerpt}</p>
-          </div>
-        </a>
+        {/* <a> */}
+        <div className={classes.image}>
+          <Image
+            src={imagePath}
+            alt={title}
+            width={300}
+            height={200}
+            layout="responsive"
+          />
+        </div>
+        <div className={classes.content}>
+          <h3>{title}</h3>
+          <time>{formattedDate}</time>
+          <p>{excerpt}</p>
+        </div>
+        {/* </a> */}
       </Link>
     </li>
   );
