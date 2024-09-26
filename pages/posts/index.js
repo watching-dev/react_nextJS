d39e7c12b1b2685a567c618352e6fb6c170c41b8
watching-dev <coderefactoring90@gmail.com>
2024-09-26 22:56:29 +0900

    [feat] allPosts 추가

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,3 +1,31 @@
-function AllPostsPage() {}
+import AllPosts from "../../components/posts/all-posts";
+
+function AllPostsPage() {
+  const DUMMY_POSTS = [
+    {
+      slug: `getting1`,
+      title: `title`,
+      image: `getting.png`,
+      excert: `Next`,
+      date: `2022-02-10`,
+    },
+    {
+      slug: `getting2`,
+      title: `title`,
+      image: `getting.png`,
+      excert: `Next`,
+      date: `2022-02-10`,
+    },
+    {
+      slug: `getting3`,
+      title: `title`,
+      image: `getting.png`,
+      excert: `Next`,
+      date: `2022-02-10`,
+    },
+  ];
+
+  return <AllPosts posts={DUMMY_POSTS} />;
+}
 
 export default AllPostsPage;
