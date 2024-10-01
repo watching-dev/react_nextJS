721a48b151b79e4aa028812688cca1965ef6b117
watching-dev <coderefactoring90@gmail.com>
2024-10-01 18:26:19 +0900

    [feat] npm install react-markdown --> 마크다운 -> JSX 로 변환 라이브러리 추가

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -1,3 +1,4 @@
+import ReactMarkdown from "react-markdown";
 import classes from "./post-content.module.css";
 
 import PostHeader from "./post-header";
@@ -16,7 +17,7 @@ function PostContent() {
   return (
     <article className={classes.content}>
       <PostHeader title={DUMMY_POST.title} image={imagePath} />
-      {DUMMY_POST.content}
+      <ReactMarkdown>{DUMMY_POST.content}</ReactMarkdown>
     </article>
   );
 }
