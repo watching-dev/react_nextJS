bb9bd5c1fdf5ef7e22653f69541e83579d73c926
watching-dev <coderefactoring90@gmail.com>
2024-10-01 22:43:33 +0900

    [feat] 동적데이터 페칭 - getStaticprops, getStaticPaths 사용

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,7 +1,33 @@
 import PostContent from "../../components/posts/post-detail/post-content";
+import { getPostData, getPostsFiles } from "../../lib/posts-util";
 
-function DetailPostPage() {
-  return <PostContent />;
+function DetailPostPage(props) {
+  return <PostContent posts={props.post} />;
 }
 
 export default DetailPostPage;
+
+export function getStaticProps(context) {
+  const { params } = context;
+  const { slug } = params;
+
+  const postData = getPostData(slug);
+
+  return {
+    props: {
+      post: postData,
+    },
+    revalidate: 600,
+  };
+}
+
+export function getStaticPaths() {
+  const postFileName = getPostsFiles();
+  const slugs = postFileName.map((fileName) => fileName.replace(/\.md$/, ""));
+  return {
+    paths: slugs.map((slug) => ({
+      params: { slug: slug },
+    })),
+    fallback: false,
+  };
+}
