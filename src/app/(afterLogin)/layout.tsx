3fef98f7fff5deb77a3e650d02388ea499849ca9
watching-dev <coderefactoring90@gmail.com>
2024-10-12 00:43:08 +0900

    [feat] afterLogin layout 디자인

--- src/app/(afterLogin)/layout.tsx
+++ src/app/(afterLogin)/layout.tsx
@@ -1,12 +1,44 @@
+import Link from "next/link";
+import styles from "./layout.module.css";
+import Image from "next/image";
+import ZLogo from "../../../public/zlogo.png";
+
 export default function afterLoginLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <>
       <div>after login layout</div>
-      <div>{children}</div>
+      <div className={styles.container}>
+        <header className={styles.leftSectionWrapper}>
+          <div className={styles.leftSection}>
+            <div className={styles.leftSectionFixed}>
+              <Link className={styles.logo} href="/home">
+                <div className={styles.logoPill}>
+                  <Image src={ZLogo} alt="z-com" width={40} height={40} />
+                </div>
+              </Link>
+            </div>
+          </div>
+        </header>
+        <div className={styles.rightSectionWrapper}>
+          <div className={styles.rightSectionInner}>
+            <main className={styles.main}>{children}</main>
+            <div className={styles.rightSection}>
+              <form className={styles.search}>
+                <svg width={20} viewBox="0 0 24 24" aria-hidden="true">
+                  <g>
+                    <path d="M10.25 3.75c-3.59 0-6.5 2.91-6.5 6.5s2.91 6.5 6.5 6.5c1.795 0 3.419-.726 4.596-1.904 1.178-1.177 1.904-2.801 1.904-4.596 0-3.59-2.91-6.5-6.5-6.5zm-8.5 6.5c0-4.694 3.806-8.5 8.5-8.5s8.5 3.806 8.5 8.5c0 1.986-.682 3.815-1.824 5.262l4.781 4.781-1.414 1.414-4.781-4.781c-1.447 1.142-3.276 1.824-5.262 1.824-4.694 0-8.5-3.806-8.5-8.5z"></path>
+                  </g>
+                </svg>
+                <input type="search" />
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
