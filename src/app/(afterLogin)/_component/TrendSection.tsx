fb0ea077afae575388c0a1b88769275ce20601d6
watching-dev <coderefactoring90@gmail.com>
2024-10-14 19:08:15 +0900

    [feat] rightSection 추가

---
+++ src/app/(afterLogin)/_component/TrendSection.tsx
@@ -0,0 +1,22 @@
+import Trend from "./Trend";
+import style from "./trendSection.module.css";
+
+export default function TrendSection() {
+  return (
+    <div className={style.trendBg}>
+      <div className={style.trend}>
+        <h3>나를 위한 트렌드</h3>
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+      </div>
+    </div>
+  );
+}
--- src/app/(afterLogin)/layout.tsx
+++ src/app/(afterLogin)/layout.tsx
@@ -4,6 +4,8 @@ import Image from "next/image";
 import ZLogo from "../../../public/zlogo.png";
 import NavMenu from "./_component/NavMenu";
 import LogoutButton from "./_component/LogoutButton";
+import TrendSection from "./_component/TrendSection";
+import FollowRecommend from "./_component/FollowRecommend";
 
 export default function afterLoginLayout({
   children,
@@ -37,16 +39,25 @@ export default function afterLoginLayout({
         <div className={styles.rightSectionWrapper}>
           <div className={styles.rightSectionInner}>
             <main className={styles.main}>{children}</main>
-            <div className={styles.rightSection}>
-              <form className={styles.search}>
-                <svg width={20} viewBox="0 0 24 24" aria-hidden="true">
-                  <g>
-                    <path d="M10.25 3.75c-3.59 0-6.5 2.91-6.5 6.5s2.91 6.5 6.5 6.5c1.795 0 3.419-.726 4.596-1.904 1.178-1.177 1.904-2.801 1.904-4.596 0-3.59-2.91-6.5-6.5-6.5zm-8.5 6.5c0-4.694 3.806-8.5 8.5-8.5s8.5 3.806 8.5 8.5c0 1.986-.682 3.815-1.824 5.262l4.781 4.781-1.414 1.414-4.781-4.781c-1.447 1.142-3.276 1.824-5.262 1.824-4.694 0-8.5-3.806-8.5-8.5z"></path>
-                  </g>
-                </svg>
-                <input type="search" />
-              </form>
-            </div>
+            <section className={styles.rightSection}>
+              <div style={{ marginBottom: 60, width: "inherit" }}>
+                <form className={styles.search}>
+                  <svg width={20} viewBox="0 0 24 24" aria-hidden="true">
+                    <g>
+                      <path d="M10.25 3.75c-3.59 0-6.5 2.91-6.5 6.5s2.91 6.5 6.5 6.5c1.795 0 3.419-.726 4.596-1.904 1.178-1.177 1.904-2.801 1.904-4.596 0-3.59-2.91-6.5-6.5-6.5zm-8.5 6.5c0-4.694 3.806-8.5 8.5-8.5s8.5 3.806 8.5 8.5c0 1.986-.682 3.815-1.824 5.262l4.781 4.781-1.414 1.414-4.781-4.781c-1.447 1.142-3.276 1.824-5.262 1.824-4.694 0-8.5-3.806-8.5-8.5z"></path>
+                    </g>
+                  </svg>
+                  <input type="search" />
+                </form>
+              </div>
+              <TrendSection />
+              <div className={styles.followRecommend}>
+                <h3>팔로우 추천</h3>
+                <FollowRecommend />
+                <FollowRecommend />
+                <FollowRecommend />
+              </div>
+            </section>
           </div>
         </div>
       </div>
