417cf53436c54dab15579e7243b25b17d8b62451
watching-dev <coderefactoring90@gmail.com>
2024-10-15 12:55:21 +0900

    [feat] main 추가

--- src/app/(afterLogin)/home/layout.tsx
+++
@@ -1,3 +0,0 @@
-export default function HomeLayout() {
-  return <div>HomeLayout</div>;
-}
--- src/app/(afterLogin)/home/page.tsx
+++ src/app/(afterLogin)/home/page.tsx
@@ -1,3 +1,29 @@
+// import Post from "../_component/Post";
+import Post from "./_component/Post";
+import PostForm from "./_component/PostForm";
+import Tab from "./_component/Tab";
+import TabProvider from "./_component/TabProvider";
+import style from "./home.module.css";
+
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <main className={style.main}>
+      <TabProvider>
+        <Tab />
+        <PostForm />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+      </TabProvider>
+    </main>
+  );
 }
